8b3f4dd0b4b3567c488272769737ae0eaa3e6800
adhe48 <54255405+adhe48@users.noreply.github.com>
2019-11-26 23:44:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -297,7 +297,7 @@ const getkodevoucher = (sessionnya, uniknya, aksesnya) => new Promise((resolve, 
        
         
         console.log(chalk.green("Trying to redeem it for you boss!"))
-        const kodevoucher = "GOFOODBOBA07"
+        const kodevoucher = "GOFOODSANTAI19"
 
         const redeem = await functionredeemvoc(kodevoucher, sessionnya, uniknya, aksesnya)
         // console.log(redeem)
